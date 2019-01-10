60146435400696a4115fc9e34cd137f6734a0e02
tomi <gandalf0000@gmail.com>
2019-01-10 18:02:33 +0100

    Add content to master branch

---
+++ src/main/java/com/mainakesystems/openshift/canaryapp/MainService.java
@@ -0,0 +1,35 @@
+package com.mainakesystems.openshift.canaryapp;
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/canaryapp" })
+public class MainService extends HttpServlet {
+    
+	private static final long serialVersionUID = 8532332299687592651L;
+
+	@Override
+    protected void doGet(final HttpServletRequest request, 
+                         final HttpServletResponse response) throws ServletException, IOException {
+        
+        System.out.println("GET Output!");
+        
+        response.getWriter().println("Output GET - master Branch!!! :D");  
+        response.getWriter().flush();
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest request, 
+                          final HttpServletResponse response) throws ServletException, IOException {
+        
+        System.out.println("POST Output!");
+        
+        response.getWriter().println("Output POST - master Branch!!! :D");
+        response.getWriter().flush();
+    }
+}
